89ebb12b505637035f73cb9a7f3883d6d5b36180
zhang.cheng <786127150@qq.com>
2025-12-16 11:46:44 +0800

    Add basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/web-tools' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/web-tools/' : '',
   images: {
     unoptimized: true
   }
